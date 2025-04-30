b7592e360d65ba77ad00be49bc14b5d23530b5c9
Pavel Mishin <pavel.mishinfz@gmail.com>
2025-05-01 01:48:24 +0300

    Update structure Modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,19 +1,43 @@
 import React from 'react';
-import '../css/Modal.css'; // Подключаем стили для модального окна
+import '../css/Modal.css';
 
-const Modal = ({ isOpen, onClose, onConfirm, message }) => {
+const Modal = ({
+    title = 'Заголовок',
+    isOpen,
+    onClose,
+    onCloseTxt = 'Отмена',
+    onConfirm,
+    onConfirmTxt = 'Подтвердить',
+    message,
+    images
+}) => {
     if (!isOpen) return null;
 
     return (
         <div className="modal-overlay">
             <div className="modal-content">
-                <p>{message}</p>
+            <h3 className='modal-header'>{title}</h3>
+                {message && (
+                    <p>{message}</p>
+                )}
+                {images && images.length > 0 && (
+                    <div className="modal-images">
+                        {images.map(({ image_id, preview }) => (
+                            <img
+                                key={image_id}
+                                src={preview}
+                                alt={`preview-${image_id}`}
+                                className="preview-image"
+                            />
+                        ))}
+                    </div>
+                )}
                 <div className="modal-buttons">
                     <button onClick={onConfirm} className="modal-btn confirm-btn">
-                        Подтвердить
+                        {onConfirmTxt}
                     </button>
                     <button onClick={onClose} className="modal-btn cancel-btn">
-                        Отмена
+                        {onCloseTxt}
                     </button>
                 </div>
             </div>
